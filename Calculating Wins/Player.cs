9a8577f76759f38fcd798973bd5a555034b64b8e
Rewan Khaled <83522163+roka4556@users.noreply.github.com>
2022-05-12 17:48:44 +0200

    Add project files.

---
+++ Calculating Wins/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculating_Wins
+{
+    class Player
+    {
+        public int games;
+        public int wins;
+        //winrate is private
+        private int winrate;
+
+        //complete the method
+        public int GetWinRate()
+        {
+            winrate = wins * 100 / games;
+            return winrate;
+            
+        }
+    }
+}
---
+++ Calculating Wins/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculating_Wins
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int games = Convert.ToInt32(Console.ReadLine());
+            int wins = Convert.ToInt32(Console.ReadLine());
+
+            //creating the player object
+            Player player1 = new Player();
+            player1.games = games;
+            player1.wins = wins;
+
+            //output
+            Console.WriteLine(player1.GetWinRate());
+        }
+    }
+}
